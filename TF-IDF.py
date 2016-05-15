88a2fad4697680ea4365afb3412fb4552606d8e7
simplymarcello <simplymarcello@gmail.com>
2016-05-15 10:54:12 -0400

    Fully computes TF-IDF correctly with PySpark/MapReduce **Not-Finished: Still need to finish computing Cos similarity**

--- TF-IDF.py
+++ TF-IDF.py
@@ -1,42 +1,59 @@
-from pyspark import SparkContext
-from pyspark.mllib.feature import HashingTF
-from pyspark.mllib.feature import IDF
+from __future__ import print_function
+import os
+import sys
 from pprint import pprint
+from operator import add
+import pyspark
+from pyspark.context import SparkContext
+import numpy as np
+import scipy.sparse as sps
+from pyspark.mllib.linalg import Vectors
+import numpy
+from math import log
 import pandas
 
-sc = SparkContext()
-file = "SampleData.txt"
-termSet = set()
-hashVals = {}
 D = {}
-docs = []
+sc = SparkContext()
+file = "SampleData3.txt"
 
-documents = sc.textFile(file).map(lambda line: line.split(" ")[1:])
+WordFrequencyDocs = sc.textFile(file, use_unicode=False)\
+                    .map(lambda l: ((l.split(" ")[0], len([x for x in l.split(" ")[1:] if ("gene_" in x or "disease_" in x)])), [x for x in l.split(" ")[1:] if ("gene_" in x or "disease_" in x)]))\
+                    .flatMap(lambda l: [((x, l[0]), 1) for x in l[1]]).cache()
+WordFrequencyDocs = WordFrequencyDocs.reduceByKey(add).cache()
+WordFrequencyDocs = WordFrequencyDocs.flatMap(lambda l: [((l[0][0]), (l[0][1][0], l[1], l[0][1][1]))]).cache()
+WordCountCorpus = WordFrequencyDocs.countByKey()
+WordFrequencyCorpus = WordFrequencyDocs.map(lambda l: [(l[0], l[1][0]), (l[1][1], l[1][2], WordCountCorpus[l[0]])]).cache()
+WordFrequencyCorpus.cache()
+Count = WordFrequencyCorpus.map(lambda l: l[0][1]).distinct().count()
 
-with open(file, 'r') as f:
-    for line in f:
-        docs.append(line.split(" ")[0].strip(' '))
+# finished TFIDF by using spark
+TFIDF = WordFrequencyCorpus.map(lambda l: [(l[0][0], l[0][1]), ((float(l[1][0])/float(l[1][1]))*(log(((Count)/(l[1][2])),10)))]).cache()
 
-hashingTF = HashingTF()
+# start building vectors to compute Cos Similarity (term-term score)
+DL = [x[0][1] for x in TFIDF.filter(lambda x: x[0][0]=='gene_nmdars_gene').collect()]
+WL = [x for x in TFIDF.filter(lambda x: x[0][1] in DL).map(lambda x: x[0][0]).distinct().collect()]
+DD = {x:TFIDF.filter(lambda l: l[0][0]==x).map(lambda x: (x[0][1].strip('doc'), x[1])).collect() for x in WL}
 
-for x in documents.collect():
-    for y in x:
-        termSet.add(y)
+# Dictonary of term that are contained in at least one of the same documents as gene_nmdars_gene
+pprint(DD)
 
-tf = hashingTF.transform(documents)
-tf.cache()
-idf = IDF().fit(tf)
-tfidf = idf.transform(tf)
+#Potentially useable snippets for later on.
+# LL = [(int(y.strip('doc')),z) for ((x,y),z) in nmdars.collect()]
+# nmdars = TFIDF.filter(lambda l: l[0][0]=='gene_mhtt_gene')
+# LL = [(int(y.strip('doc')),z) for ((x,y),z) in nmdars.collect()]
+# print(sorted(LL))# VD = Vectors.dense(LL)
+# print(VD)
+# nmdarsDocs = nmdars.map(lambda l: l[0])
+# TFIDF = TFIDF.map(lambda l: [(l[0][0]), [(l[0][1],l[1])]]).reduceByKey(add)
+# TFIDF = TFIDF.filter(lambda l: (l[0]=="gene_nmdars_gene" or l[0]=="gene_ctr_gene"))
+# TFIDF.map(lambda l: []*[]).sum()
+# pprint(sorted(TFIDF.collect()))
+# for x in sorted(TFIDF.collect()):
+#     tmp = D.get(x[0], {})
+#     for (y,z) in x[1]:
+#         tmp[y] = z
+#     D[x[0]] = tmp
+# DF = pandas.DataFrame(D)
+# tfq = DF.transpose().ix[:,['gene_calcitonin_gene','gene_ctr_gene']]
+# print(tfq.dropna(thresh=1))
 
-for x in termSet:
-    hashVals[hashingTF.indexOf(str(x))] = str(x)
-
-for x, y in zip(docs, tfidf.collect()):
-    for w, z in zip([hashVals[u] for u in y.indices], y.values):
-        tmp = D.get(x, {})
-        tmp[w] = z
-        D[x] = tmp
-
-DF = pandas.DataFrame(D)
-sc.stop()
-print(DF.transpose())
